09f9610767bea77986e40fb7461feeb574337939
Harsh056-star <hnagpal056@gmail.com>
2025-08-01 23:55:41 +0530

    Button installed by shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home(){
   return(
-    <h1 className="text-red-600">Hello World</h1>
+    <Button>Click Me</Button>
+    // <h1 className="text-red-600">Hello World</h1>
   )
 }
