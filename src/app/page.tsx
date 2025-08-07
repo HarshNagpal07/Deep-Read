01cfc2b275b46a910bc2314f3a7ae8ec70b4d1e3
Harsh056-star <hnagpal056@gmail.com>
2025-08-07 14:24:33 +0530

    Settle the bg and h1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 import { Button } from "@/components/ui/button";
 
-export default function Home(){
+export default async function Home(){
   return(
-    <Button>Click Me</Button>
-     
+    <div className="w-screen min-h-screen bg-gradient-to-r from-red-100 to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className="flex flex-col items-center text-center">
+        <div className="flex items-center">
+          <h1 className="mr-3 text-5xl font-semibold">Chat any PDF</h1>
+        </div>
+      </div>
+      </div>
+    </div>
   )
 }
